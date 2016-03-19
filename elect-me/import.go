f0407adb6f18b638d960fac51c17617179f74890
ramjac <ramjacvp@gmail.com>
2016-03-19 15:28:01 -0400

    Initial code commit. Mainly focused on the html/css. We can use the go code if we feel like it. I based it off some tutorials and the RAP project.

---
+++ elect-me/import.go
@@ -0,0 +1,12 @@
+package electme
+
+/*
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func csvimport() {
+	db, err := sql.Open("mysql", "user@cloudsql(project-id:instance-name)/dbname")
+}
+*/
